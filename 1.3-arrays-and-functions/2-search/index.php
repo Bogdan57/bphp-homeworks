5826414b99cdd33dcb20689099e795aa9d1cef9c
Bogdan Novikov <fikis57@mail.ru>
2019-08-30 15:00:29 +0300

    add last fix

--- 1.3-arrays-and-functions/2-search/index.php
+++ 1.3-arrays-and-functions/2-search/index.php
@@ -1,39 +1,36 @@
 <?php
-function generate($rows, $placesPerRow, $avaliableCount) {
-    $seats = [];
-    for($i = 0; $i < $rows; $i++){
-        for($j = 0; $j < $placesPerRow; $j++) {
-            $seats[$i][$j] = false;
-        } 
-    }
-    if ($rows * $placesPerRow > $avaliableCount) {
-        return false;
-    } else {
-        return $seats;
-    }
-};
-function reserve(&$map, $row, $place) {
-    if($map[$row - 1][$place - 1] === false) {
-        $map[$row - 1][$place - 1] = true;
-        return true;
-    } else {
+function generate($rows, $places, $chairs) {
+    if ($rows * $places > $chairs) {
         return false;
     }
-    
+    $map = [];
+    for ($i = 0; $i < $rows; $i++) {
+        for ($j = 0; $j < $places; $j++) {
+            $map[$i][$j] = false;
+        }
+    }
+    return $map;
+}
+function tryReserve ($map, $requiredPlaces) {
+    for ($i = 0; $i < count($map); $i++) {
+        for ($j = 0; $j < count($map[$i]); $j++) {
+            if (($j + $requiredPlaces) > (count($map[$i]))) {
+                return false;
+            }
+            if ($map[$i][$j] === false) {
+                $requiredPlaces--;
+                if ($requiredPlaces === 0) {
+                    return 'Найдены лучшие места: c '.($j-$requiredPlaces+2).' по '.($j+1).' в ряду '.($i+1);
+                }
+            }
+        }
+    }
+    return 'Подходящих мест не найдено';
 }
 $chairs = 50;
 $map = generate(5, 8, $chairs);
-$requireRow = 3;
-$requiredPlace = 5;
-$reverve = reserve($map, $requireRow, $requiredPlace);
-logReserve($requireRow, $requiredPlace, $reverve);
-$reverve = reserve($map, $requireRow, $requiredPlace);
-logReserve($requireRow, $requiredPlace, $reverve);
-function logReserve($row, $place, $result){
-    if ($result) {
-        echo "Ваше место забронировано! Ряд $row, место $place".PHP_EOL;
-    } else {
-        echo "Что-то пошло не так=( Бронь не удалась".PHP_EOL;
-    }
-}
+$map[1][7] = true;
+$map[4][2] = true;
+$requiredPlaces = 3;
+echo tryReserve($map, $requiredPlaces);
 ?>

209069b73ea361fa1bfd540450d14a44558f28d9
Bogdan Novikov <fikis57@mail.ru>
2019-09-12 21:13:24 +0300

    add 1 and 2

--- 3.2-PHP7-PSR-PHPDoc/3.2.2/createOrder.php
+++ 3.2-PHP7-PSR-PHPDoc/3.2.2/createOrder.php
@@ -5,12 +5,11 @@
  * @var array $menu
  * @var array $post
  */
-
 require_once 'const.php';
 require_once 'loadJSON.php';
 require_once 'renderView.php';
-require_once 'PoschitatSchet.php';
+require_once 'countBill.php';
 $menu = loadJSON('menu');
+$services = loadJSON('services');
 $post = $_POST;
-
-renderView('default','order', [ 'order' => PoschitatSchet($menu, $post)]);
+renderView('default', 'order', ['order' => countBill($menu, $services, $post)]);
